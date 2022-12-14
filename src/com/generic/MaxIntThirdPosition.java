af4f91d82b7bbb9338caa76fc9e12923ac6f7346
jayashri23 <jsjankar23@gmail.com>
2022-12-14 20:17:00 +0530

    Finding Max at Third Position

---
+++ src/com/generic/MaxIntThirdPosition.java
@@ -0,0 +1,39 @@
+package com.generic;
+
+public class MaxIntThirdPosition {
+    public static class Maximum<T extends Comparable<T>> {
+        T x, y, z;
+
+        public Maximum(T x, T y, T z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public T maximum() {
+            return com.generic.MaximumIntFirst.maximum(x, y, z);
+        }
+
+        //determine max number out of three
+        public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+            T max = x;  // assume x is max at initially
+            if (y.compareTo(max) > 0) {
+                max = y;   //y is max so far
+            }
+            if (z.compareTo(max) > 0) {
+                max = z;
+            }
+            System.out.println(max);
+            return max;
+        }
+
+        public static void main(String[] args) {
+            System.out.println("...............Third Position.............");
+            System.out.println("Maximum Number out Of Three Integer:");
+            Integer x = 5, y = 2, z = 10;
+            new com.generic.MaximumIntFirst(x, y, z).maximum();
+        }
+    }
+}
+
+
