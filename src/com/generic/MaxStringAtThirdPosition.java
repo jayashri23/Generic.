3c3699f7a410a5d2bc789ddc578110fd073da568
jayashri23 <jsjankar23@gmail.com>
2022-12-14 20:40:00 +0530

    Finding Max String  at third  Position

---
+++ src/com/generic/MaxStringAtThirdPosition.java
@@ -0,0 +1,36 @@
+package com.generic;
+
+public class MaxStringAtThirdPosition {
+    public static void main(String[] args) {
+        System.out.println("...............Maximum String At Third Position.............");
+        System.out.println("Maximum String out Of Three String:");
+        String x = "Apple", y = "Banana", z = "Peach";
+        new com.generic.MaximumIntFirst(x, y, z).maximum();
+    }
+    public class Maximum<T extends Comparable<T>> {
+        T x, y, z;
+
+        public Maximum(T x, T y, T z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public T maximum() {
+            return com.generic.MaximumIntFirst.maximum(x, y, z);
+        }
+
+        //determine max number out of three
+        public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+            T max = x;  // assume x is max at initially
+            if (y.compareTo(max) > 0) {
+                max = y;   //y is max so far
+            }
+            if (z.compareTo(max) > 0) {
+                max = z;
+            }
+            System.out.println(max);
+            return max;
+        }
+    }
+}
